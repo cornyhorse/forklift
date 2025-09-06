25b5a7ff5f7d9cc5b681ef8a31b31bbf1aa76ba1
cornyhorse <matt@mattkingsbury.com>
2025-09-06 06:25:12 -0400

    Enhance case handling in _fix_case_issues method to better manage acronyms and exceptions

--- src/forklift/utils/transformations/string_transformations.py
+++ src/forklift/utils/transformations/string_transformations.py
@@ -324,22 +324,56 @@ class StringTransformer:
 
     def _fix_case_issues(self, text: str, title_case_exceptions: list, acronyms: list) -> str:
         """Fix common case issues."""
-        # Simple implementation - can be expanded
+        # Don't process short text (less than 3 characters) or text that's not all uppercase
+        if len(text) <= 2 or not text.isupper():
+            return text
+
+        # Default common acronyms that should remain uppercase
+        default_acronyms = {
+            'NASA', 'FBI', 'CIA', 'USA', 'UK', 'US', 'CEO', 'CTO', 'CFO', 'VP',
+            'HR', 'IT', 'AI', 'API', 'URL', 'HTTP', 'HTTPS', 'SQL', 'HTML',
+            'CSS', 'JS', 'XML', 'JSON', 'PDF', 'CSV', 'ZIP', 'HTTP', 'FTP',
+            'TCP', 'IP', 'DNS', 'SSL', 'TLS', 'AWS', 'IBM', 'AMD', 'GPU',
+            'CPU', 'RAM', 'SSD', 'HDD', 'USB', 'DVD', 'CD', 'TV', 'HD', 'UHD'
+        }
+
+        # Combine default acronyms with custom ones
+        all_acronyms = default_acronyms.copy()
+        if acronyms:
+            all_acronyms.update(acronym.upper() for acronym in acronyms)
+
         # Fix multiple consecutive uppercase letters (except known acronyms)
         words = text.split()
         fixed_words = []
 
-        for word in words:
+        for i, word in enumerate(words):
+            # Remove punctuation for checking exceptions/acronyms
+            word_clean = ''.join(c for c in word if c.isalpha())
+
             # Check if word is a known acronym
-            if word.upper() in [a.upper() for a in acronyms]:
-                fixed_words.append(word.upper())
-            elif len(word) > 1 and word.isupper():
-                # Convert to title case unless it's an exception
-                if word.lower() in title_case_exceptions:
-                    fixed_words.append(word.lower())
-                else:
-                    fixed_words.append(word.title())
+            if word_clean.upper() in all_acronyms:
+                # Preserve acronym case but handle punctuation
+                result = ""
+                for char in word:
+                    if char.isalpha():
+                        result += char.upper()
+                    else:
+                        result += char
+                fixed_words.append(result)
+            elif i == 0:
+                # First word is always capitalized (title case), even if it's an exception
+                fixed_words.append(word.title())
+            elif word_clean.lower() in title_case_exceptions:
+                # Use lowercase for exception words (but not the first word)
+                result = ""
+                for char in word:
+                    if char.isalpha():
+                        result += char.lower()
+                    else:
+                        result += char
+                fixed_words.append(result)
             else:
-                fixed_words.append(word)
+                # Convert to title case
+                fixed_words.append(word.title())
 
         return ' '.join(fixed_words)
